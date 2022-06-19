607ca9232d7f3bd0a358a3f49bacb2589fb99010
datalater <the7mincheol@gmail.com>
2022-06-20 00:42:43 +0900

    6.1 지역변수의 값을 변경할 때

--- 6.1-extract-function/main.js
+++ 6.1-extract-function/main.js
@@ -1,46 +1,49 @@
 function printOwing(invoice) {
   let result = "\n";
-  let outstanding = 0;
-
   result += printBanner();
 
-  // 미해결 채무(outstanding)를 계산한다.
+  const outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+
+  result += printDetails(invoice, outstanding);
+  return result;
+}
+
+function calculateOutstanding(invoice) {
+  let outstanding = 0;
+
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
-  recordDueDate(invoice);
+  return outstanding;
+}
 
-  result += printDetails(invoice, outstanding);
+function recordDueDate(invoice) {
+  const today = new Date(0);
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+}
 
-  function recordDueDate(invoice) {
-    const today = new Date(0);
-    invoice.dueDate = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate() + 30
-    );
-  }
+function printDetails(invoice, outstanding) {
+  let result = "";
 
-  function printDetails(invoice, outstanding) {
-    let result = "";
+  result += `고객명: ${invoice.customer}\n`;
+  result += `채무액: ${outstanding}\n`;
+  result += `마감일: ${invoice.dueDate.toLocaleDateString()}\n`;
 
-    result += `고객명: ${invoice.customer}\n`;
-    result += `채무액: ${outstanding}\n`;
-    result += `마감일: ${invoice.dueDate.toLocaleDateString()}\n`;
+  return result;
+}
 
-    return result;
-  }
+function printBanner() {
+  let result = "";
 
-  function printBanner() {
-    let result = "";
-
-    result += "*****************\n";
-    result += "**** 고객 채무 ****\n";
-    result += "*****************\n";
-
-    return result;
-  }
+  result += "*****************\n";
+  result += "**** 고객 채무 ****\n";
+  result += "*****************\n";
 
   return result;
 }
