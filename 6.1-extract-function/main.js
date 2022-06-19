5c2b602e08d89a4cf6facd8c3e722fea59bd10f4
datalater <the7mincheol@gmail.com>
2022-06-20 00:35:52 +0900

    6.1 지역변수를 사용할 때
    다른 값을 다시 대입하지 않는다면 매개변수로 넘기면 됩니다.

--- 6.1-extract-function/main.js
+++ 6.1-extract-function/main.js
@@ -9,17 +9,20 @@ function printOwing(invoice) {
     outstanding += o.amount;
   }
 
-  // 마감일(dueDate)을 기록한다.
-  const today = new Date(0);
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  recordDueDate(invoice);
 
-  result += printDetails();
+  result += printDetails(invoice, outstanding);
 
-  function printDetails() {
+  function recordDueDate(invoice) {
+    const today = new Date(0);
+    invoice.dueDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
+    );
+  }
+
+  function printDetails(invoice, outstanding) {
     let result = "";
 
     result += `고객명: ${invoice.customer}\n`;
