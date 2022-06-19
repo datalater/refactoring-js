13172484debb1023cf0acce0df760734163f5d13
datalater <the7mincheol@gmail.com>
2022-06-20 00:31:46 +0900

    6.1 유효범위를 벗어나는 변수가 없을 때

--- 6.1-extract-function/main.js
+++ 6.1-extract-function/main.js
@@ -2,9 +2,7 @@ function printOwing(invoice) {
   let result = "\n";
   let outstanding = 0;
 
-  result += "*****************\n";
-  result += "**** 고객 채무 ****\n";
-  result += "*****************\n";
+  result += printBanner();
 
   // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
@@ -19,10 +17,27 @@ function printOwing(invoice) {
     today.getDate() + 30
   );
 
-  // 세부 사항을 출력한다.
-  result += `고객명: ${invoice.customer}\n`;
-  result += `채무액: ${outstanding}\n`;
-  result += `마감일: ${invoice.dueDate.toLocaleDateString()}\n`;
+  result += printDetails();
+
+  function printDetails() {
+    let result = "";
+
+    result += `고객명: ${invoice.customer}\n`;
+    result += `채무액: ${outstanding}\n`;
+    result += `마감일: ${invoice.dueDate.toLocaleDateString()}\n`;
+
+    return result;
+  }
+
+  function printBanner() {
+    let result = "";
+
+    result += "*****************\n";
+    result += "**** 고객 채무 ****\n";
+    result += "*****************\n";
+
+    return result;
+  }
 
   return result;
 }
